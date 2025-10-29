e7752d0a34990f4719a8a358f8748c1504621b90
Autopopcornshooter <70250405+Autopopcornshooter@users.noreply.github.com>
2025-10-29 11:59:04 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/runnityWeb/RunnityWebApplication.java
@@ -0,0 +1,13 @@
+package runnityWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnityWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnityWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/runnityWeb/RunnityWebApplicationTests.java
@@ -0,0 +1,13 @@
+package runnityWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnityWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
