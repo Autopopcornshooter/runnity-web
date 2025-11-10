22bb2583d7c6db54c53b70d1a0da54bfc94b4f35
Autopopcornshooter <70250405+Autopopcornshooter@users.noreply.github.com>
2025-11-10 11:17:32 +0900

    fix: 회원정보 수정 오류 수정 및 패스워드 인코딩 기능 추가

--- src/main/java/runnity/service/UserService.java
+++ src/main/java/runnity/service/UserService.java
@@ -26,88 +26,91 @@ import runnity.util.CustomSecurityUtil;
 @Slf4j
 public class UserService {
 
-    private final UserRepository userRepository;
-    private final RegionRepository regionRepository;
-    private final PasswordEncoder encoder;
+  private final UserRepository userRepository;
+  private final RegionRepository regionRepository;
+  private final PasswordEncoder encoder;
 
-    public User saveUserInfo(SignUpRequest request) {
+  public User saveUserInfo(SignUpRequest request) {
 
-        Region region = regionRepository.findByLatAndLng(request.getLat(), request.getLng())
-                .orElseGet(() -> {
-                    Region newRegion = Region.builder()
-                            .address(request.getAddress())
-                            .lat(request.getLat())
-                            .lng(request.getLng())
-                            .build();
-                    return regionRepository.save(newRegion);
-                });
+    Region region = regionRepository.findByLatAndLng(request.getLat(), request.getLng())
+        .orElseGet(() -> {
+          Region newRegion = Region.builder()
+              .address(request.getAddress())
+              .lat(request.getLat())
+              .lng(request.getLng())
+              .build();
+          return regionRepository.save(newRegion);
+        });
 
-        return userRepository.save(
-                User.builder()
-                        .nickname(request.getNickname())
-                        .loginId(request.getUsername())
-                        .password(encoder.encode(request.getPassword()))
-                        .createdAt(request.getCreatedAt())
-                        .updatedAt(request.getUpdatedAt())
-                        .runnerLevel(request.getRunnerLevel())
-                        .region(region)
-                        .userRole(UserRole.ROLE_USER)
-                        .matchState(UserMatchState.IDLE)
-                        .build()
+    return userRepository.save(
+        User.builder()
+            .nickname(request.getNickname())
+            .loginId(request.getUsername())
+            .password(encoder.encode(request.getPassword()))
+            .createdAt(request.getCreatedAt())
+            .updatedAt(request.getUpdatedAt())
+            .runnerLevel(request.getRunnerLevel())
+            .region(region)
+            .userRole(UserRole.ROLE_USER)
+            .matchState(UserMatchState.IDLE)
+            .build()
+    );
+  }
+
+  public void updateUserInfo(UpdateUserInfoRequest request) {
+    User user = authenticatedUser();
+    if (request.getUsername() != null && !request.getUsername().isBlank()) {  //입력값이 있을 경우
+      user.setLoginId(request.getUsername());
+    }
+    if (request.getPassword() != null && !request.getPassword().isBlank()) { //입력값이 있을 경우
+      user.setPassword(encoder.encode(request.getPassword()));
+    }
+    if (request.getNickname() != null && !request.getNickname().isBlank()) {
+      user.setNickname(request.getNickname());
+    }
+    user.setUpdatedAt(LocalDateTime.now());
+    userRepository.save(user);
+    Authentication newAuth =
+        new UsernamePasswordAuthenticationToken(
+            user, user.getPassword(), user.getAuthorities()
         );
+    SecurityContextHolder.getContext().setAuthentication(newAuth);
+    log.info("UserInfo changed");
+    log.info("User Login Id : {} password : {}", authenticatedUser().getLoginId(),
+        authenticatedUser().getPassword());
+
+  }
+
+  public void updateRunnerLevel(RunnerLevelRequest request) {
+    User user = authenticatedUser();
+    user.setRunnerLevel(request.getRunnerLevel());
+    userRepository.save(user);
+    if (request.getRunnerLevel().equals(user.getRunnerLevel())) {
+      log.info("러너 레벨 변경 완료: {}", user.getRunnerLevel().name());
+    } else {
+      log.error("러너 레벨 변경 실패");
     }
+  }
 
-    public void updateUserInfo(UpdateUserInfoRequest request) {
-        User user = authenticatedUser();
-        if (!request.getUsername().isBlank()) {  //입력값이 있을 경우
-            user.setLoginId(request.getUsername());
-        }
-        if (!request.getPassword().isBlank()) { //입력값이 있을 경우
-            user.setPassword(request.getPassword());
-        }
-        user.setNickname(request.getNickname());
-        user.setUpdatedAt(LocalDateTime.now());
-        userRepository.save(user);
-        Authentication newAuth =
-                new UsernamePasswordAuthenticationToken(
-                        user, user.getPassword(), user.getAuthorities()
-                );
-        SecurityContextHolder.getContext().setAuthentication(newAuth);
-        log.info("UserInfo changed");
-        log.info("User Login Id : {} password : {}", authenticatedUser().getLoginId(), authenticatedUser().getPassword());
-
-    }
-
-    public void updateRunnerLevel(RunnerLevelRequest request) {
-        User user = authenticatedUser();
-        user.setRunnerLevel(request.getRunnerLevel());
-        userRepository.save(user);
-        if (request.getRunnerLevel().equals(user.getRunnerLevel())) {
-            log.info("러너 레벨 변경 완료: {}", user.getRunnerLevel().name());
-        } else {
-            log.error("러너 레벨 변경 실패");
-        }
-    }
-
-    public boolean isLoginIdExist(String loginId) {
-        return userRepository.existsByLoginId(loginId);
-    }
+  public boolean isLoginIdExist(String loginId) {
+    return userRepository.existsByLoginId(loginId);
+  }
 
 
-    public boolean isNicknameExist(String nickName) {
-        return userRepository.existsByNickname(nickName);
-    }
+  public boolean isNicknameExist(String nickName) {
+    return userRepository.existsByNickname(nickName);
+  }
 
-    public void deleteUserInfo(Long id) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new UserNotFoundException(id));
-        userRepository.delete(user);
-    }
+  public void deleteUserInfo(Long id) {
+    User user = userRepository.findById(id)
+        .orElseThrow(() -> new UserNotFoundException(id));
+    userRepository.delete(user);
+  }
 
-    public User authenticatedUser() {
-        return userRepository.findByLoginId(CustomSecurityUtil.getCurrentUserLoginId())
-                .orElseThrow(() -> new UserNotFoundException(CustomSecurityUtil.getCurrentUserLoginId()));
-    }
+  public User authenticatedUser() {
+    return userRepository.findByLoginId(CustomSecurityUtil.getCurrentUserLoginId())
+        .orElseThrow(() -> new UserNotFoundException(CustomSecurityUtil.getCurrentUserLoginId()));
+  }
 
 
 }
